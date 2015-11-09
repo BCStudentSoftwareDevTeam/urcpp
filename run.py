800253ece9376da44c30b189980d85a62e1e6370
Matt Jadud <matt@jadud.com>
2015-11-09 09:15:22 -0500

    Fix.

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
-from otp import app
+from web import app
 import os
 
+# Run with 
+# $ IP=0.0.0.0 PORT=8080 python run.py 
+# or similar
 app.run(host = os.getenv('IP'), port = int(os.getenv('PORT')), debug = True)
