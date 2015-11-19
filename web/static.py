8fde031fa04053f8bc7d9f04df0c7623ccec12c6
Matt Jadud <matt@jadud.com>
2015-11-19 15:59:45 -0500

    Deleting code related to the "web" version.
    This removes all of the old code that was part of
    my initial explorations of URCPP/Flask.
    
    The API directory is now where all good things happen.

--- web/static.py
+++
@@ -1,8 +0,0 @@
-# Our standard import soup
-from everything import *
-
-@app.route('/s/<path:path>')
-def static_proxy(path):
-  # send_static_file will guess the correct MIME type
-  return app.send_static_file(path)
-
