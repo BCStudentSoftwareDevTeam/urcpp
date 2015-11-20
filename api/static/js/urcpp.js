eae317352afcabdc8e4ee15e8ae56d69beb5390d
Matt Jadud <matt@jadud.com>
2015-11-19 19:35:26 -0500

    Renamed.
    Nothing more, really. Well, tweaked files to match new naming.

--- api/static/js/urcpp.js
+++ api/static/js/urcpp.js
@@ -1,41 +1,15 @@
-// See both:
-// * https://github.com/mattdesl/module-best-practices
-// * https://css-tricks.com/how-do-you-structure-javascript-the-module-pattern-edition/
-
 var urcpp = function (version) {
-  var h = {
-    client: null,
-
-    init: function (version) {
-      console.log("Creating a version '" + version + "' client.");
-
-      var c = new $.RestClient('/urcpp/' + version + '/');
-      // Establish all of our endpoints.
-      c.add('get', { isSingle: true });
-      c.add('set', { isSingle: true });
-      c.get.add('facultydetails', { stripTrailingSlash: true});
-      c.show();
-
-      h.client = c;
-    },
-
-    // All functions should take a single dictionary (object)
-    // as an argument. It would be good if we used consistent
-    // keys for those arguments. For the moment, I'm going
-    // to use good commenting practice to make sure the
-    // programmer knows which keys to include...
-
-    // PARAMS
-    //  username: string
-    //  data: dictionary
-    // PURPOSE
-    // Gets faculty details.
-    fd: function (params) {
-      h.client.get.facultydetails.create (params.username, params.data).done (params.done);
+  var h =  {
+    faculty: {
+      details: function (username, fun) {
+        aja()
+          .method ('POST')
+          .url ('/urcpp/' + version + '/faculty/details/' + username)
+          .body ( { username: username })
+          .on ('success', fun).go();
+      }
     }
-  };
-
-  h.init(version);
+  }
 
   return h;
-}
+};
