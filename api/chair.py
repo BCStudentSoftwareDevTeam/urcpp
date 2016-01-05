5fabca58d21793d4124a428a970b5813315a664a
Matthew Jadud <jadudm@gmail.com>
2016-01-05 19:56:54 +0000

    Adding new files for admin interface

---
+++ api/chair.py
@@ -0,0 +1,22 @@
+from everything import *
+from faculty import getFaculty, getLDAPFaculty
+from projects import getProject
+from programs import getAllPrograms
+from budget import getBudget
+from parameters import getParameters
+
+from pages import *
+
+@app.route("/<username>/chair", methods = ["GET"])
+def chair_GET (username):
+  if username != authUser(request.environ):
+    return { "response": cfg["response"]["badUsername"] }
+  parameters = getParameters()
+  ldap = getLDAPFaculty(username)
+  
+  return render_template ("chair.html", 
+                           username = username,
+                           ldap = ldap,
+                           params = parameters,
+                           cfg = cfg,
+                           )
