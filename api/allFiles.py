d7e2d89590052d943e703b3f042cf62fc1ccdb5c
Matthew Jadud <jadudm@gmail.com>
2016-01-29 00:29:20 +0000

    del folders after 30mins on page load

--- api/allFiles.py
+++ api/allFiles.py
@@ -10,10 +10,14 @@ from flask import send_file
 
 from pages import *
 
-import os, pprint, zipfile, uuid
+import os, pprint, zipfile, uuid, time
 import shutil
 
+# remove all folders that are not user folders and are older than one minute
+
+
 @app.route("/<username>/committee/allFiles", methods = ["GET"])
+
 def allFiles_GET (username):
   if username != authUser(request.environ):
     return { "response": cfg["response"]["badUsername"] }
@@ -24,6 +28,10 @@ def allFiles_GET (username):
   budget = getAllBudgets()
   parameters = getParameters()
   prevFilepath = {}
+  
+  yearDir = cfg["filepaths"]["projectFiles"]+str(parameters.year)
+  projectDir = cfg["filepaths"]["projectFiles"]
+  
   for fac in faculty:
     prevFilepath[fac.username.username] = {}
     for uploadType in ["narrative", "vitae", "irb"]: 
@@ -31,9 +39,19 @@ def allFiles_GET (username):
         prevFilepath[fac.username.username][uploadType]= checkForFile(fac.username.username, uploadType)
 
   # Does the zipping
-  shutil.make_archive("api/static/files/" + str(parameters.year), 'zip', 'api/static/files/' + str(parameters.year))
+  shutil.make_archive(yearDir, 'zip', yearDir)
+    
+  allFolders = os.walk(yearDir).next()[1]
+  allFac = [fac.username.username for fac in faculty]
   
-  
+  print allFolders
+  print allFac
+
+  for folder in allFolders:
+    fullPath =  yearDir + "/" + str(folder)
+    if (folder not in allFac) and (os.stat(fullPath).st_mtime < time.time() - 60*30):
+      shutil.rmtree(fullPath)
+    
 
   return render_template (  "allFiles.html",
                             proj = proj,
@@ -49,7 +67,7 @@ def allFiles_GET (username):
 @app.route("/<username>/committee/allFiles", methods = ["POST"])
 def allFiles_POST (username):
   if username != authUser(request.environ):
-    return { "response": cfg["response"]["badUsername"] }
+    return { "response": projectDir}
   # All of our queries
   faculty = getFacultyWithProjects()
   proj = getAllProjects()
@@ -57,6 +75,10 @@ def allFiles_POST (username):
   budget = getAllBudgets()
   parameters = getParameters()
   prevFilepath = {}
+  
+  yearDir = cfg["filepaths"]["projectFiles"]+str(parameters.year)
+  pfojectDir = cfg["filepaths"]["projectFiles"]
+  
   for fac in faculty:
     prevFilepath[fac.username.username] = {}
     for uploadType in ["narrative", "vitae", "irb"]: 
@@ -67,20 +89,18 @@ def allFiles_POST (username):
   uniqueID = uuid.uuid1() 
   
   try:
-    os.stat(cfg["filepaths"]["projectFiles"]+str(parameters.year))
-    print("it exits"+ os.getcwd())
+    os.stat(yearDir)
   except:
-    print ("making dir...")
-    os.mkdir(cfg["filepaths"]["projectFiles"]+str(parameters.year))
+    os.mkdir(yearDir)
     ##############
     # http://stackoverflow.com/questions/273192/in-python-check-if-a-directory-exists-and-create-it-if-necessary
     ##############
-  os.mkdir(cfg["filepaths"]["projectFiles"]+str(parameters.year)+"/"+str(uniqueID))
+  os.mkdir(yearDir+"/"+str(uniqueID))
   
   facultyPending = getFacultyWithPendingProjects() 
 
   for fac in facultyPending:
-    path = cfg["filepaths"]["projectFiles"]+str(parameters.year)+"/"+str(uniqueID)+"/project"+str(fac.pID.pID)
+    path = yearDir + "/"+str(uniqueID)+"/project"+str(fac.pID.pID)
     os.mkdir(path)
     for uploadType in ["narrative", "vitae", "irb"]:
             
@@ -98,25 +118,7 @@ def allFiles_POST (username):
   
   temp = uuid.uuid1()
     
-  print "###############################" + 'attachment; filename="'+ cfg["filepaths"]["projectFiles"] + str(parameters.year)+"/"+ str(temp) + "/" + str(parameters.year)+".zip\""
-  os.mkdir(cfg["filepaths"]["projectFiles"]+str(parameters.year)+"/"+str(temp))
-  shutil.make_archive(cfg["filepaths"]["projectFiles"] + str(parameters.year)+"/"+ str(temp) + "/" + str(parameters.year), 'zip', cfg["filepaths"]["projectFiles"] + "/" + str(parameters.year)+"/"+str(uniqueID))            
+  os.mkdir(yearDir+"/"+str(temp))
+  shutil.make_archive(yearDir+"/"+ str(temp) + "/" + str(parameters.year), 'zip', yearDir +"/"+str(uniqueID))            
   
-  # response = make_response()
-  # response.headers['Cache-Control'] = 'no-cache'
-  # response.headers['Content-Type'] = 'application/zip'
-  # response.headers['Content-Disposition'] = 'attachment; filename="'+ cfg["filepaths"]["projectFiles"] + str(parameters.year)+"/"+ str(temp) + "/" + str(parameters.year)+".zip\""
-  # return response
-  
-  return send_file("../" + cfg["filepaths"]["projectFiles"] + str(parameters.year)+"/"+ str(temp) + "/" + str(parameters.year)+".zip", as_attachment=True)
-
-  # return render_template (  "allFiles.html",
-  #                   proj = proj,
-  #                   username = username,
-  #                   cfg = cfg,
-  #                   fac = faculty,
-  #                   progs = programs,
-  #                   budg = budget,
-  #                   files = prevFilepath,
-  #                   params = parameters
-  #                 )
+  return send_file("../" + yearDir +"/"+ str(temp) + "/" + str(parameters.year)+".zip", as_attachment=True)
