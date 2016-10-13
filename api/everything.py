cf070a8f53c833765ff9436caca25e3bc41429b1
Ishwar Agarwal <a.isshwar@gmail.com>
2016-10-13 00:27:53 +0000

    forgot to uncomment secure key

--- api/everything.py
+++ api/everything.py
@@ -51,7 +51,7 @@ from api.config import load_config
 from api.authTool import AuthorizedUser
 # cfg = load_config('/var/www/html/urcpp-flask/api/config.yaml')
 cfg = load_config(os.path.join(here, 'config.yaml'))
-#app.config['SECRET_KEY'] = open(os.path.join(here, 'secret_key'), 'rb').read()
+app.config['SECRET_KEY'] = open(os.path.join(here, 'secret_key'), 'rb').read()
 @app.before_request
 def before_request():
     g.dbDynamic = dynamicDB.connect()
