7ab89cc6a5c681b584e66efc03c2a5031d901243
Guillermo Ramos-Macias <guillermoramos330179@gmail.com>
2016-10-16 14:09:28 +0000

    remove unused variable and imports

--- api/allLabor.py
+++ api/allLabor.py
@@ -1,8 +1,5 @@
 from everything import *
 from faculty import getFacultyWithProjects
-from projects import getAllCurrentProjects
-from programs import getAllPrograms
-from budget import getAllBudgets
 from parameters import getParameters
 from makeExcel import getFilename
 from applicationCycle import getCurrentCycle
@@ -12,23 +9,18 @@ from pages import *
 def allLabor_GET (username):
   if username != authUser(request.environ):
     return { "response": cfg["response"]["badUsername"] }
+  # TODO: choose get parameter or application year
   # we need the year so that we can get the current projects
   applicationCycle =  getCurrentCycle()
   # All of our queries
   faculty = getFacultyWithProjects(applicationCycle.year)
-  project = getAllCurrentProjects()
-  programs = getAllPrograms()
-  budget = getAllBudgets()
   parameters = getParameters()
   downloadFileName = getFilename("allLabor")
   
   return render_template (  "allLabor.html",
-                            proj = project,
                             username = username,
                             cfg = cfg,
                             fac = faculty,
-                            progs = programs,
-                            budg = budget,
                             params = parameters,
                             downloadFileName = downloadFileName
                           )
