830aa27bd785c87ff6b18f1f7cadf4a4dc0bc9d4
Guillermo Ramos-Macias <guillermoramos330179@gmail.com>
2017-01-17 14:22:23 +0000

    add ui to let the chair decide what the current parameter is

--- api/static/js/pages/setParameters.js
+++ api/static/js/pages/setParameters.js
@@ -1,4 +1,4 @@
-/* global $ */ 
+/* global $, swal */ 
 $(function() {
       $("#applicationOpenDate").datepicker({
         dateFormat: "yy-mm-dd"
@@ -10,4 +10,61 @@ $(function() {
     });
     
 $('.selectpicker').selectpicker({
+});
+
+function change_check_color(parameters_id){
+  $(".isCurrentParameter").addClass("disabled");
+  
+  $("#set_current_parameters-"+parameters_id).removeClass("disabled")
+}
+
+ function set_current_parameters(parameters_id) {
+    swal({
+  title: "Are you sure?",
+  text: "This will change the current cycle to be under these parameters",
+  type: "warning",
+  showCancelButton: true,
+  confirmButtonColor: "#DD6B55",
+  confirmButtonText: "Confirm",
+  cancelButtonText: "Cancel",
+  closeOnConfirm: true,
+  closeOnCancel: false
+},
+function(isConfirm){
+  if (isConfirm) {
+    $.get('/set/current_parameters/'+ parameters_id, function( data ){
+    change_check_color(parameters_id);
+    });
+  } else {
+    swal("Cancelled", "Everythig is normal");
+  }
+});
+
+  };
+  
+function warnBeforeRedirect(url) {
+  swal({
+  title: "Are you sure?",
+  text: "This will delete these parameters and may cause unexpected behavior",
+  type: "warning",
+  showCancelButton: true,
+  confirmButtonColor: "#DD6B55",
+  confirmButtonText: "Confirm",
+  cancelButtonText: "Cancel",
+  closeOnConfirm: false,
+  closeOnCancel: false
+},
+function(isConfirm){
+  if (isConfirm) {
+    location.replace(url);
+  } else {
+    swal("Cancelled", "Everythig is normal");
+  }
+});
+}
+  
+$("#delete_parameters").click(function(e) {
+  e.preventDefault();
+  var linkURL = $(this).attr("href");
+  warnBeforeRedirect(linkURL);
 });
