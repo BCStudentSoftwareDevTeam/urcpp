67754f45397598517b055a21b68abd8dffbc6f2e
Guillermo Ramos-Macias <guillermoramos330179@gmail.com>
2017-01-19 13:53:24 +0000

    add docstring for remove files function

--- api/API/files.py
+++ api/API/files.py
@@ -3,8 +3,14 @@ from parameters import getCurrentParameters
 from ..everything import *
 
 def removeFiles(username):
+  """ This function removes the folder that belong to a user. 
+      It should be used when the project is withdrawn
+      
+      Args:
+        username (str): The username of the faculty whose folder should be deleted
+  
+  """
   currentParameter = getCurrentParameters()
-  print base_path
   folder_path = os.path.join(base_path, cfg["filepaths"]["projectFiles"], str(currentParameter.year), username)
   shutil.rmtree(folder_path)
   
