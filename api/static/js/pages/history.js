810f31df97f63cf150ee9254cd8df9e4e8e16acb
Guillermo Ramos-Macias <guillermoramos330179@gmail.com>
2017-01-19 13:17:05 +0000

    diable submit button if nothing is checked

--- api/static/js/pages/history.js
+++ api/static/js/pages/history.js
@@ -1,11 +1,25 @@
+/* global $ */
 $(document).ready(function() {
-    
+    change_button();
     $(".checkbox_array").click(function() {
-        var atLeastOneIsChecked = $('input[type=checkbox]:checked').length > 0;
+        change_button();
+    });
+});
+function atLeastOneIsCheckedFromFaculty(){
+    var faculty_check = $('.primary > .checkbox > label > input[type=checkbox]:checked').length > 0;
+    
+    var collaborators =  $(".collab_checks");
+    var areAllCollabsChecked = false;
+    collaborators.each(function(index, value) {
+        
+    });
+}
+function change_button() {
+var atLeastOneIsChecked = $('input[type=checkbox]:checked').length > 0;
         if (atLeastOneIsChecked) {
             $("#submit").removeAttr("disabled");
         } else {
             $("#submit").prop("disabled", true);
         }
-    })
-})
+        
+}
