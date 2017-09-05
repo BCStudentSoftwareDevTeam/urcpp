146f8946a6ce365b173be5bf929ccb3456b9ce0b
Jesson Soto <jessons13@gmail.com>
2017-09-05 19:45:50 +0000

    Delete Files working, but interface needs work

--- api/pages/upload.py
+++ api/pages/upload.py
@@ -59,6 +59,23 @@ def removeLeadingDot (line):
   line = re.sub('[.]', '', line)
   return line
 
+@app.route('/upload/removefile/<uploadType>/<username>', methods=['POST'])
+def remove_file(username, uploadType):
+  if username != authUser(request.environ):
+    return { "response": cfg["response"]["badUsername"] }
+  else:
+    rawpath = cfg["filepaths"]["directory"]
+    cycle   = getCurrentParameters()
+    rawpath = rawpath.replace("%%applicationCycle%%", str(cycle.year))
+    path    = rawpath.replace("%%username%%", username)
+    path    = os.path.join(base_path, path)
+    previous_file = checkForFile(username, uploadType, cycle.year)
+
+    if previous_file:
+      app.logger.info("{0} removing {1}.".format(username, path))
+      os.remove(os.path.join(path, previous_file))
+      return "File Deleted"
+  return "Error, File Not Deleted"
 # Captures file upload from the dropzone and saves to server
 @app.route('/v1/upload/<whichfile>/<username>', methods=['POST'])
 def upload_file(whichfile, username):
