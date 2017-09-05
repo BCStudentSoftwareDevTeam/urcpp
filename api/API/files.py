853c5fa257331f62616c46b7bedf16add4a212d8
Sher Sanginov <sanginovs@berea.edu>
2017-09-05 19:43:20 +0000

    send email to user

--- api/API/files.py
+++ api/API/files.py
@@ -1,6 +1,11 @@
 import shutil
 from parameters import getCurrentParameters
 from ..everything import *
+#Flask mail imports
+from flask_mail import Mail, Message
+
+
+
 
 def removeFiles(username):
   """ This function removes the folder that belong to a user. 
@@ -13,4 +18,26 @@ def removeFiles(username):
   currentParameter = getCurrentParameters()
   folder_path = os.path.join(base_path, cfg["filepaths"]["projectFiles"], str(currentParameter.year), username)
   shutil.rmtree(folder_path)
+  
+
+def send_email(user_email, app=None):
+  '''this function sends email to the user email
+  specify port, mail-server, username, password in settings.py
+  '''
+  mail=Mail(app)
+  msg=Message("Hello",
+    sender="from@example.com",
+    recipients=[user_email] 
+  )
+  msg.body="this is a messsage body"
+  mail.send(msg)
+  
+@app.route("/email")
+def email():
+  '''this route is for testing purposes'''
+  my_email="heggens@berea.edu"
+  send_email(my_email, app)
+  return "Message is sent."
+  
+
   
---
+++ api/settings.py
@@ -0,0 +1,6 @@
+MAIL_SERVER='your-server',
+MAIL_PORT=25,
+MAIL_USE_SSL=True,
+DEBUG=True,
+MAIL_USERNAME="your_email@example.com",
+MAIL_PASSWORD="your-password"
