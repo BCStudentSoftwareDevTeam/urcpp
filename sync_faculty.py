9c4c0bd4ee46e350f39db3e29ced93cb357e5f7b
Jesson Soto <jessons13@gmail.com>
2017-10-02 21:57:34 +0000

    Modified Sync_Faculty to add new user to db

--- sync_faculty.py
+++ sync_faculty.py
@@ -1,71 +1,124 @@
 from ldap3 import Server, Connection, ALL
 from peewee import *
-from api.everything import *
+from os import path, remove
+from api.models import *
+import argparse
 
-cfg = load_config('api/config.yaml')
-# skt = load_config('api/secret_config.yaml')
+# Create database connection and model
+database_path = "LDAP.db"
+database = SqliteDatabase(database_path)
 
-theStaticDB = SqliteDatabase(cfg['databases']['static'])
-theStaticDB.drop_table(LDAPFaculty)
+class Faculty(Model):
+    fID               = PrimaryKeyField()
+    username          = CharField(unique = True)
+    bnumber           = TextField(null = True)
+    lastname          = TextField(null = True)
+    firstname         = TextField(null = True)
 
-server = Server ('berea.edu', port=389, use_ssl=False, get_info='ALL')
-# conn   = Connection (server, user=skt['ldap']['user'], password=skt['ldap']['pass'])
-conn   = Connection (server, user="BPLP", password="Ol1v4r!Tun4bp")
-if not conn.bind():
-    print('error in bind', conn.result)
+    class Meta:
+        database = database
 
-# search_base and search_filter are the parameters
-conn.search('dc=berea,dc=edu', 
-  '(description=Faculty)', 
-  attributes = ['samaccountname', 'givenname', 'sn', 'employeeid']
-  )
+class Staff(Model):
+    sID               = PrimaryKeyField()
+    username          = CharField(unique = True)
+    bnumber           = TextField(null = True)
+    lastname          = TextField(null = True)
+    firstname         = TextField(null = True)
+    class Meta:
+        database = database
 
-print ("Found {0} faculty.".format(len(conn.entries))) 
 
-# print (conn.entries[0])
+def connect_to_server(user,password):
+    server = Server ('berea.edu', port=389, use_ssl=False, get_info='ALL')
+    # conn   = Connection (server, user=skt['ldap']['user'], password=skt['ldap']['pass'])
+    conn   = Connection (server, user=user, password=password)
+    if not conn.bind():
+        print(conn.result)
+        raise Exception("BindError")
+    return conn
 
-def safe (d, k):
-  result = ""
-  try:
-    if k in d:
-      result = d[k]
-  # If we can't find a key, skip it.
-  except:
-    pass
-  
-  return result
+def grab_faculty(connection):
+    return grab_data(connection, "Faculty")
+
+def grab_staff(connection):
+    return grab_data(connection,"Staff")
+
+def grab_data(connection, description):
+    # search_base and search_filter are the parameters
+    connection.search('dc=berea,dc=edu',
+      '(description=%s)' % (description),
+      attributes = ['samaccountname', 'givenname', 'sn', 'employeeid']
+      )
+
+    return connection.entries
+
+def grab_key(entry, key):
+    if key in entry:
+        return entry[key]
+    else:
+        return None
+
+
+def dump_to_database(entries, Table):
+    for entry in entries:
+        row = Table(
+            username          = grab_key(entry,'samaccountname'),
+            bnumber           = grab_key(entry,'employeeid'),
+            lastname          = grab_key(entry,'sn'),
+            firstname         = grab_key(entry,'givenname')
+        )
+        row.save()
+
+
+def main(user,password):
+    pull_database(user, password)
+    print("Adding new members")
+    add_members()
     
-# Recreate the table.
-theStaticDB.create_table(LDAPFaculty)
+def add_members():
+    current_users = LDAPFaculty.select(LDAPFaculty.username)
+    user_list = list()
+    for user in current_users:
+        user_list.append(user.username)
+    new_users = Faculty.select().where( ~ (Faculty.username << user_list))
+    print("Adding %s new members" %(str(len(new_users))))
+    for user in new_users:
+        if user.bnumber is None:
+            print(user.username)
+            continue
+        
+        new_user = LDAPFaculty.create(
+                username = user.username,
+                bnumber = user.bnumber,
+                lastname = user.lastname,
+                firstname = user.firstname
+            )
+        new_user.save()
+        
+def pull_database(user,password):
+    print("Connecting to Database")
+    database.connect()
+    print("Dropping and creating tables")
+    if path.isfile(database_path):
+        try:
+            database.drop_tables([Faculty, Staff])
+        except Exception:
+            pass
+    database.create_tables([Faculty, Staff])
+    print("Connecting to server")
+    connection = connect_to_server(user,password)
+    print("Grabbing data")
+    faculty = grab_faculty(connection)
+    # staff = grab_staff(connection)
+    print("Dumping Data")
+    dump_to_database(faculty, Faculty)
+    # dump_to_database(Staff, Staff)
 
-faculty = conn.entries
-for fac in faculty:
-  print ("Faculty: {0}".format(fac.samaccountname))
-  o = LDAPFaculty (
-    lastname  = safe (fac, 'sn'),
-    firstname = safe (fac, 'givenname'),
-    username  = fac.samaccountname,
-    bnumber   = safe (fac, 'employeeid'),
-    )
-  o.save()
-  
-  
-# Now do Staff
-conn.search('dc=berea,dc=edu', 
-  '(description=Staff)', 
-  attributes = ['samaccountname', 'givenname', 'sn', 'employeeid']
-  )
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Pull and Sync Faculty from LDAP')
+    parser.add_argument('--user', help='Username for LDAP')
+    parser.add_argument('--password', help="Password for LDAP")
+    args = parser.parse_args()
+    
+    main(args.user, args.password)
 
-print ("Found {0} staff.".format(len(conn.entries))) 
-
-
-faculty = conn.entries        #actually staff, but who cares?
-for fac in faculty:
-  print ("Staff: {0}".format(fac.samaccountname))
-  o = LDAPFaculty (
-    lastname  = safe (fac, 'sn'),
-    firstname = safe (fac, 'givenname'),
-    username  = fac.samaccountname,
-    bnumber   = safe (fac, 'employeeid'),
-    )
-  o.save()
