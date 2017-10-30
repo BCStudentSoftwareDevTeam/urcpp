07112aa865aeb91cc86f40333e3498ea1713798f
Guillermo ramos <guillermoramos330179@gmail.com>
2017-10-30 22:36:33 +0000

    Remove unneeded fields from migrating

--- migrate.py
+++ migrate.py
@@ -40,16 +40,3 @@ emails = EmailTemplates (
                   )
 emails.save(force_insert=True)
 
-migrator = MySQLMigrator(dynamicDB)
-
-# Create your field instances. For non-null fields you must specify a
-# default value.
-isChair     = BooleanField(default=False)
-isCommitteeMember = BooleanField(default=False)
-
-# Run the migration, specifying the database table, field name and field.
-migrate(
-    migrator.add_column('ldapfaculty', 'isChair', isChair),
-    migrator.add_column('ldapfaculty', 'isCommitteeMember', isCommitteeMember),
-)
-
