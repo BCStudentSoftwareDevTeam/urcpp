7c24542ba5226b106c3463827dc258f3da7c8375
Guillermo Ramos-Macias <guillermoramos330179@gmail.com>
2018-02-12 17:09:50 +0000

    show accepted email on email button

--- api/chair/awardLetters.py
+++ api/chair/awardLetters.py
@@ -89,3 +89,32 @@ def awardLetters_generate(username,pID):
       return {"mail_to": "Failed to send email to: %s" % (email_address)}
 
   return jsonify({"mail_to":"Email sent to: %s"  % (email_address) })
+
+@app.route("/chair/awardLetters/get/<pID>", methods = ["GET"])
+@login_required
+def accept_letters_get(pID):
+  if not g.user.isCommitteeMember:
+    abort(403)
+  # All of our queries
+  
+  project = Projects.get(Projects.pID == pID)
+  # we need the current year to get current faculty with projects
+  template = EmailTemplates.get(EmailTemplates.eID == 1)
+  body = template.Body
+  subject = template.Subject
+  funding = str(getTotalBudget(project.budgetID))
+  project_title = project.title
+  student_count = str(project.numberStudents)
+  start = str(project.startDate.strftime("%B %d, %Y"))
+  end = str(project.endDate.strftime("%B %d, %Y"))
+  stipend = str(project.budgetID.facultyStipend)
+  
+  # Replace all placeholder text
+  body = body.replace("@@Funding@@",funding)
+  body = body.replace("@@ProjectTitle@@",project_title)
+  body = body.replace("@@Students@@",student_count)
+  body = body.replace("@@Start Date@@",start)
+  body = body.replace("@@End Date@@",end)
+  body = body.replace("@@Stipend@@",stipend)
+  
+  return jsonify({"body": body, "subject":subject})
