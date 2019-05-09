b90ecf63417520d84bede5cb9c1631f5253683d6
Kat Adams <adamska@berea.edu>
2019-05-09 18:27:08 +0000

    No negative numbers for rates

--- api/static/js/pages/setParameters.js
+++ api/static/js/pages/setParameters.js
@@ -70,6 +70,7 @@ $("#delete_parameters").click(function(e) {
 });
 
 function addEvent(node, type, callback) {
+  //Step 1 of form validation
   if (node.addEventListener) {
     node.addEventListener(type, function(e) {
       callback(e, e.target);
@@ -77,6 +78,51 @@ function addEvent(node, type, callback) {
   } else if (node.attachEvent) {
     node.attachEvent('on' + type, function(e) {
       callback(e, e.srcElement);
+    });
+  }
+}
+
+function shouldBeValidated(field) {
+  //Step 2 of form validation which simply tests that it’s neither disabled nor readonly, and that it has either a pattern or a required attribute
+  return (
+    !(field.getAttribute("readonly") || field.readonly) &&
+    !(field.getAttribute("disabled") || field.disabled) &&
+    (field.getAttribute("pattern") || field.getAttribute("required"))
+  );
+}
+
+function instantValidation(field) {
+  //Step 3 of form validation which tests the field and then performs the actual validation
+  if (shouldBeValidated(field)) {
+    //the field is invalid if:
+    //it's required but the value is empty
+    //it has a pattern but the (non-empty) value doesn't pass
+    var invalid =
+      (field.getAttribute("required") && !field.value) ||
+      (field.getAttribute("pattern") &&
+        field.value &&
+        !new RegExp(field.getAttribute("pattern")).test(field.value));
+    if (!invalid && field.getAttribute("aria-invalid")) {
+      field.removeAttribute("aria-invalid");
+    } else if (invalid && !field.getAttribute("aria-invalid")) {
+      field.setAttribute("aria-invalid", "true");
+    }
+  }
+}
+
+addEvent(document, "change", function(e, target) {
+  instantValidation(target);
+});
+
+//step4: now bind a change event to each applicable for field
+var fields = [
+  document.getElementsByTagName("input"),
+  document.getElementsByTagName("textarea")
+];
+for (var a = fields.length, i = 0; i < a; i++) {
+  for (var b = fields[i].length, j = 0; j < b; j++) {
+    addEvent(fields[i][j], "change", function(e, target) {
+      instantValidation(target);
     });
   }
 }
