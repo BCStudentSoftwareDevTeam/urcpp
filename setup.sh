54b5254057828a60ab1ec1718d5e0d8305b7b663
Kat Adams <adamska@berea.edu>
2019-05-21 19:55:33 +0000

    the user input will be moved to settings.py as strings.

--- setup.sh
+++ setup.sh
@@ -53,9 +53,9 @@ echo "Please Fill out the information"
 read  -p 'MAIL_SERVER= ' mail_server_var
 read  -p 'MAIL_PORT= ' mail_port_var
 read  -p 'MAIL_DEFAULT_SENDER= ' mail_default_sender_var
-sed -i 's/MAIL_SERVER=/MAIL_SERVER='$mail_server_var'/g' api/settings.py
-sed -i 's/MAIL_PORT=/MAIL_PORT='$mail_port_var'/g' api/settings.py
-sed -i 's/MAIL_DEFAULT_SENDER=/MAIL_DEFAULT_SENDER='$mail_default_sender_var'/g' api/settings.py
+sed -i 's/MAIL_SERVER=/MAIL_SERVER="'$mail_server_var'"/g' api/settings.py
+sed -i 's/MAIL_PORT=/MAIL_PORT="'$mail_port_var'"/g' api/settings.py
+sed -i 's/MAIL_DEFAULT_SENDER=/MAIL_DEFAULT_SENDER="'$mail_default_sender_var'"/g' api/settings.py
 else 
    touch $FILE
    echo "$FILE created"
@@ -63,9 +63,9 @@ else
    read  -p 'MAIL_SERVER= ' mail_server_var
    read  -p 'MAIL_PORT= ' mail_port_var
    read  -p 'MAIL_DEFAULT_SENDER= ' mail_default_sender_var
-   sed -i 's/MAIL_SERVER=/MAIL_SERVER='$mail_server_var'/g' api/settings.py
-   sed -i 's/MAIL_PORT=/MAIL_PORT='$mail_port_var'/g' api/settings.py
-   sed -i 's/MAIL_DEFAULT_SENDER=/MAIL_DEFAULT_SENDER='$mail_default_sender_var'/g' api/settings.py
+   sed -i 's/MAIL_SERVER=/MAIL_SERVER="'$mail_server_var'"/g' api/settings.py
+   sed -i 's/MAIL_PORT=/MAIL_PORT="'$mail_port_var'"/g' api/settings.py
+   sed -i 's/MAIL_DEFAULT_SENDER=/MAIL_DEFAULT_SENDER="'$mail_default_sender_var'"/g' api/settings.py
 fi
 
 # TODO Enter information to fill in the file in interactive mode in shell
