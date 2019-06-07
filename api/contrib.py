c40e1bade07143151837ae1002a8f9a940da1bbc
Kat Adams <adamska@berea.edu>
2019-06-07 14:20:51 +0000

    finished the contributors page with all the people who have worked on it.

--- api/contrib.py
+++ api/contrib.py
@@ -1,9 +1,6 @@
 from everything import *
-import os
 
-print("Contributors page is loaded")
-@app.route("/contrib", methods = ["GET"])
+
+@app.route("/contributor", methods = ["GET"])
 def contributors():
-    print("Here's my print sttaement")
-    print("Current directory: " + os.getcwd())
     return render_template("contrib.html", cfg = cfg)
