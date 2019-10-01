97d3017017d46ad945c9e3492c8db5bd04e34ddb
Brian Ramsay <ramsayb2@berea.edu>
2019-10-01 10:39:13 -0400

    Load all requirements, not just a few

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 # Source this.
 # Setup virtualenv
-mkdir -p data
+#mkdir -p data
 
 if [ ! -d venv ]
 then
@@ -9,40 +9,11 @@ fi
 
 . venv/bin/activate
 
-pip install Flask
-pip install peewee
-pip install pyyaml
-# For security, for LDAP
-pip install pyopenssl ndg-httpsclient pyasn1
-pip install ldap3
-# For docs
-pip install mkdocs
-pip install gunicorn
-pip install tornado
+pip install -r requirements.txt
 
-# additional python library
-pip install XlsxWriter
-
-#for login
-pip install Flask-Login
-pip install PyMySQL
-# For QR Codes
-pip install flask-wtf
-pip install wtf-peewee
-#flask-mail library
-pip install Flask-Mail
-#pip install sqlite-web
-#pip install pyotp
-#pip install qrcode
-#pip install Pillow
-#pip install git+git://github.com/ojii/pymaging.git#egg=pymaging
-#pip install git+git://github.com/ojii/pymaging-png.git#egg=pymaging-png
-
-# Database setup
-# python recreate_dynamic.py
+# TODO create a db script that can use the docker init builder
 
 # Set up config files
-
 FILE=${PWD}api/secret_key
 if [ ! -f "$FILE" ]; then
 	echo "Generating secret key"
