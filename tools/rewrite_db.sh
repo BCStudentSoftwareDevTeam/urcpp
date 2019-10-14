e0f66aec61a4721df697e111f604f21f55666d1e
Brian Ramsay <ramsayb2@berea.edu>
2019-10-14 15:18:39 -0400

    Add better visibility to database updates

--- tools/rewrite_db.sh
+++ tools/rewrite_db.sh
@@ -19,21 +19,28 @@ pushd ../database/docker_init > /dev/null
 rm $DATA_LINK
 cp ../$DATA_FILE $DATA_LINK
 popd > /dev/null
-echo "Done"
+echo "done"
 
 # If we want to mess with the docker db
 if [ "$TARGET" == "docker" ] ; then
 	printf "Removing existing database volume..."
 	sudo rm -r ../docker/data/db/*
 
-	printf "Done.\n\nStop your docker containers. The next 'docker-compose up' will load the new database.\n"
+	printf "done.\n\nStop your docker containers. The next 'docker-compose up' will load the new database.\n"
 
 # if we are using mysql locally, run the sql files directly
 else
 	MYSQL_CONN="--defaults-extra-file=mysql.conf"
 
-	echo "Creating backup in tools/backup.sql. Remove if not needed."
+	printf "Creating backup in tools/backup.sql..."
 	mysqldump $MYSQL_CONN urcpp_flask > backup.sql
+	echo "done. Remove if not needed."
+
+	printf "Dropping existing user and database..."
 	echo "DROP USER urcpp_flask; DROP DATABASE urcpp_flask;" | mysql $MYSQL_CONN
+	echo "done"
+
+	printf "Reinitializing database from database/docker_init/*.sql..."
 	cat ../database/docker_init/*.sql | mysql $MYSQL_CONN
+	echo "done"
 fi
