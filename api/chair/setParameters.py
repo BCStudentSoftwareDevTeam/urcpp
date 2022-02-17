44ddd6b9525f9763b7a6cf6e9a32305631ff916b
Luis Riera <rieral@berea.edu>
2022-02-17 16:13:09 -0500

    Changed 11 to 23 as the system works on a 24 hour clock instead of a 12 hour clock

--- api/chair/setParameters.py
+++ api/chair/setParameters.py
@@ -14,29 +14,29 @@ def setParameters_GET ():
     openDate = datetime.datetime.strptime(data['applicationOpenDate'], '%Y-%m-%d')
     closeDate = (datetime.datetime
                          .strptime(data['applicationCloseDate'], '%Y-%m-%d')
-                         .replace(hour=11, minute=55) )
+                         .replace(hour=23, minute=55) )
     parameters = Parameters(year = int(data['newYear']),
                             appOpenDate = openDate,
                             appCloseDate =closeDate,
                             mileageRate = data['mileageRate'],
                             laborRate = data['laborRate'])
-    
+
     parameters.save()
-    
+
     flash('New application year succesfully created')
     return redirect(url_for('setParameters_GET', username = g.user.username))
-    
+
   parameters = getCurrentParameters()
   parameters_list = Parameters.select().order_by(-Parameters.year)
-  
-  return render_template ("chair/setParameters.html", 
+
+  return render_template ("chair/setParameters.html",
                            username = g.user.username,
                            ldap = g.user,
                            params = parameters,
                            parameters_list = parameters_list,
                            cfg = cfg,
                            )
-                           
+
 @app.route("/delete/parameters/<pID>", methods=['GET'])
 @login_required
 def deleteParameters(pID):
@@ -45,9 +45,7 @@ def deleteParameters(pID):
   try:
     parameters = Parameters.get(Parameters.pID == pID)
     parameters.delete_instance()
-  
+
   except Parameters.DoesNotExist:
     flash('Parameters not found')
   return redirect(redirect_url())
-  
-  
